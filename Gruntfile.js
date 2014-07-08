d6542f90f2ba6d019911651574d32bb59cbe4a70
Albert Bloomfield <albert.bloomfield@ampelectricvehicles.com>
2014-07-08 09:25:53 -0400

    update grunt tasks a good deal

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,47 +2,105 @@ module.exports = function(grunt) {
 
 	var fs = require('fs');
 	var path = require('path');
+	var child_process = require('child_process');
+	var jsyaml = require('js-yaml');
 	var pdfsYml = "";
+	var pdfsJson = {};
+
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+
+	grunt.initConfig({
+		clean: {
+			master: ["**/*.log", "**/*.aux", "**/*.gaux"],
+			ghpages: ["gh-pages/**/*.pdf"]
+		},
+		copy: {
+			pdfs: {
+				expand: true,
+				src: ["**/*.pdf", "!gh-pages/**/*", "!**/*test.pdf", "!**/test*", ],
+				dest: "gh-pages/pdfs/",
+				options: {
+					// process: function(content, srcpath) {
+					// 	var cat = convertName(path.dirname(srcpath));
+					// 	if(!pdfsJson[cat]) pdfsJson[cat] = [];
+					// 	var name = convertName(path.basename(srcpath, '.pdf'));
+					// 	var fpath = path.join('pdfs/', srcpath);
+					// 	var newObj = {name: name, url: fpath};
+					// 	var idx = findInsertionPoint(pdfsJson[cat], newObj, numericSortNew);
+					// 	pdfsJson[cat].splice(idx, 0, newObj);
+					// 	return content;
+					// }
+				}
+			}
+		},
+		custom: {
+			pdfs: {
+				expand: true,
+				src: ["**/*.pdf", "!gh-pages/**/*", "!**/*test.pdf", "!**/test*", ],
+			}
+		},
+		gabc: {
+			build: {
+				expand: true,
+				src: ['**/*.gabc']
+			}
+		},
+		tex: {
+			build: {
+				expand: true,
+				src: ['**/*.tex', '!psalms/**/*', '!template*/**/*', '!**/??--*', '!**/vr*', '!**/lesson?-*', '!**/Ant?-*', '!**/MagnificatAntiphon*'
+				, '!**/Psalm*', '!**/*-MagAntiphon*', '!**/*vr.tex', '!**/*-test.tex', '!**/test.tex']
+			}
+		}
+	});
+
+	/**
+	 * Find insertion point for a value val, as specified by the comparator 
+	 * (a function)
+	 * @param sortedArr The sorted array
+	 * @param val The value for which to find an insertion point (index) in the array
+	 * @param comparator The comparator function to compare two values
+	 */
+	function findInsertionPoint(sortedArr, val, comparator) {   
+	   var low = 0, high = sortedArr.length;
+	   var mid = -1, c = 0;
+	   while(low < high)   {
+	      mid = parseInt((low + high)/2);
+	      c = comparator(sortedArr[mid], val);
+	      if(c > 0)   {
+	         low = mid + 1;
+	      }else if(c < 0) {
+	         high = mid;
+	      }else {
+	         return mid;
+	      }
+	      //alert("mid=" + mid + ", c=" + c + ", low=" + low + ", high=" + high);
+	   }
+	   return low;
+	}
 
 	function ensureDir(dir) {
 		if(dir.length === '') return;
-		try {
-			var stat = fs.statSync(dir);
-			if(stat.isDirectory()) return;
-		} catch(ex) {}
+		if(grunt.file.isDir(dir)) return;
 		ensureDir(path.dirname(dir));
-		fs.mkdirSync(dir);
+		grunt.file.mkdir(dir);
 	}
 
-	function copyFile(source, target, cb) {
-		grunt.log.write(source + '\n');
-
-		ensureDir(path.dirname(target));
-
-		fs.writeFileSync(target, fs.readFileSync(source));
-	}
-
-	function addCategory(dirname) {
-		if(pdfsYml.length > 0) pdfsYml += "\n";
-		pdfsYml += dirname + ":" + "\n";
-	}
 	function strSplice(str, index, count, add) {
 		return str.slice(0, index) + add + str.slice(index + count);
 	}
 	function convertName(filename) {
-		return filename.replace(/([A-Z][a-z]*|\d*)/g, "$1 ");
-	}
-	function addItem(fpath) {
-		var name = path.basename(fpath, '.pdf');
-		fpath = path.join('pdfs/', fpath);
-		pdfsYml += " - name: " + convertName(name) + "\n";
-		pdfsYml += "   url: " + fpath + "\n";
-		//grunt.log.write(name + '\n');
+		return filename.replace(/-main/, '')
+			.replace(/([a-z\-]+|\d+)/g, "$1 ").trim();
 	}
 	function writePdfsYml() {
-		fs.writeFileSync('gh-pages/_data/pdfs.yml', pdfsYml);
+		grunt.file.write('gh-pages/_data/pdfs.yml', pdfsYml);
 	}
 
+	function numericSortNew(a, b) {
+		return(numericSort(a.name, b.name));
+	}
 	function numericSort(a, b) {
 		var numa = a.match(/\d+/);
 		var numb = b.match(/\d+/);
@@ -52,39 +110,26 @@ module.exports = function(grunt) {
 		return a < b? 1: a === b? 0: -1;
 	}
 
-	function listDirs(root) {
-		var files = fs.readdirSync(root).sort(numericSort);
-		var categoryAdded = false;
+	grunt.registerMultiTask('custom', 'debug stuff', function() {
+		for (var i = this.filesSrc.length - 1; i >= 0; i--) {
+			var srcpath = this.filesSrc[i];
+			var cat = convertName(path.dirname(srcpath));
+			if(!pdfsJson[cat]) pdfsJson[cat] = [];
+			var name = convertName(path.basename(srcpath, '.pdf'));
+			var fpath = path.join('pdfs/', srcpath);
+			var newObj = {name: name, url: fpath};
+			var idx = findInsertionPoint(pdfsJson[cat], newObj, numericSortNew);
+			pdfsJson[cat].splice(idx, 0, newObj);
+		};
 
-		for (var i = files.length - 1; i >= 0; i--) {
-			var fn = root + '/' + files[i];
-			var stats = fs.statSync(fn);
-			if(stats.isFile()) {
-				if(files[i].slice(-4).toLowerCase() == ".pdf") {
-					// it is a PDF
-					if(!categoryAdded) {
-						addCategory(path.basename(root));
-						categoryAdded = true;
-					}
+		grunt.file.write('gh-pages/_data/pdfs.yaml', jsyaml.safeDump(pdfsJson));
+		grunt.log.ok();
+	});
 
-					copyFile(fn, "gh-pages/pdfs" + fn.slice(1), function(err) {
-						grunt.log.write('F ' + fn + '\n');
-						if(err) grunt.log.error(err);
-					});
-					addItem(fn);
-				}
-			} else if(stats.isDirectory()) {
-				//grunt.log.write('D ' + fn + '\n');
-				if(files[i][0] !== '.'
-					&& files[i] !== 'gh-pages'
-				) {
-					listDirs(fn);
-				}
-			}
-		}
-	}
+	grunt.registerTask('default', ['copy', 'custom']);
 
-	grunt.registerTask('default', 'Log some stuff', function() {
+	/*grunt.registerTask('default', 'Log some stuff', function() {
+		var name = this.name || 'copyFiles'
 		/*var files = fs.readdirSync(".");
 		for (var i = files.length - 1; i >= 0; i--) {
 			var stats = fs.statSync(files[i]);
@@ -92,9 +137,68 @@ module.exports = function(grunt) {
 			if(stats.isDirectory()) grunt.log.write('D ');
 			grunt.log.write(files[i] + '\n');
 		}*/
-		listDirs('.');
+		/*var targets = taskrun.init(name, {target: target});
 		writePdfsYml();
 		grunt.log.ok();
+	});*/
+
+	function processTexFile(files, done) {
+		var index = 0;
+		while(index < files.length) {
+			if(path.extname(files[index]) === '.tex') {
+				var tex = child_process.spawn('lualatex', ['--interaction=batchmode', files[index]]);
+				tex.stdout.on('data', function(data) {
+					grunt.log.write(data);
+				});
+				tex.stderr.on('data', function(data) {
+					grunt.log.write(data);
+				});
+				tex.on('close', function(code) {
+					grunt.log.write('exited with code ' + code + '\n');
+					return processTexFile(files, index + 1, done);
+				});
+			}
+			++index;
+		}
+		done();
+	};
+
+	function runFiles(cmd, args, files, done) {
+		var idx = 0;
+		var absPath = path.resolve('.');
+		args.push('');
+		grunt.log.writeln(absPath);
+		function doneFunction() {
+			if(idx >= files.length) {
+				done(true);
+				return;
+			}
+			//grunt.log.writeln(files[idx]);
+			var dir = path.join(absPath, path.dirname(files[idx]));
+			var file = path.basename(files[idx]);
+			grunt.log.writeln(dir);
+			grunt.log.writeln(file);
+			grunt.file.setBase(dir);
+			args[args.length-1] = file;
+			grunt.util.spawn({cmd: cmd, args:args}, doneFunction);
+			++idx;
+		}
+
+		doneFunction();
+	}
+
+	grunt.registerMultiTask('tex', 'build tex files', function() {
+		var done = this.async();
+		runFiles('lualatex', ['--interaction=batchmode'], this.filesSrc, done);
+		//this.filesSrc.forEach(function(file) {
+		//	grunt.log.writeln(file);
+		//});
+		//processTexFile(files, 0, done);
+	});
+
+	grunt.registerMultiTask('gabc', 'build gabc files', function() {
+		var done = this.async();
+		runFiles('gregorio', [], this.filesSrc, done);
 	});
 
 	// // Project configuration
