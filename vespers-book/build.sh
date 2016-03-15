d46f23a20b44470f6dc20c9e7eb8778691e57748
Albert Bloomfield <abloomfield@gmail.com>
2016-03-15 15:29:18 -0400

    update build scripts to allow forcing build

--- vespers-book/build.sh
+++ vespers-book/build.sh
@@ -1,9 +1,39 @@
-./ly-build.sh
+function usage
+{
+	echo "usage: build.sh [-f] [-l] [-t] [-g]"
+	echo "       -f force all"
+	echo "       -l force lilypond"
+	echo "       -t force tex"
+	echo "       -g force ghostscript"
+}
 
-latexmk vespers-book-main.tex
+force=
+latexmkopts=
+lyopts=
+
+while [ "$1" != "" ]; do
+	case $1 in
+		-f | --force )			force=1
+								latexmkopts=-g
+								lyopts=-f
+								;;
+		-l | --force-lilypond )	lyopts=-f
+								;;
+		-t | --force-tex )		latexmkopts=-g
+								;;
+		-g | --force-gs )		force=1
+								;;
+		* )						usage
+								exit 1
+	esac
+	shift
+done
+
+./ly-build.sh $lyopts
+
+latexmk vespers-book-main.tex $latexmkopts
 if [ "$?" -eq "0" ]; then
-	if test `find "vespers-book-main.pdf" -mmin -1`
-	then
+	if test `find "vespers-book-main.pdf" -mmin -1` || ! [ -f vespers-book.pdf ] || [ "$force" = "1" ]; then
 		gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=vespers-book.pdf vespers-book-main.pdf &
 	fi
 fi
--- vespers-book/ly-build.sh
+++ vespers-book/ly-build.sh
@@ -1,12 +1,32 @@
 shopt -s nullglob
 
+function usage
+{
+	echo "usage: ly-build.sh [-f]"
+}
+
+echo $*
+
+force=
+
+while [ "$1" != "" ]; do
+	case $1 in
+		-f | --force )		force=1
+							;;
+		* )					usage
+							exit 1
+	esac
+	shift
+done
+
+
 pushd ../MarianAntiphons
 
 for i in *.ly; do
     ext="${i##*.}"
     fname="${i%.*}"
     echo ${fname}
-    if [ "${fname}.pdf" -ot "${fname}.ly" ]; then
+    if [ "${fname}.pdf" -ot "${fname}.ly" ] || [ "$force" = "1" ]; then
         lilypond -dno-point-and-click -dgui --bigpdfs "${fname}.ly" &
     fi
 done
