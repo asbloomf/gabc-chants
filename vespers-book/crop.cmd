740dceae61e1e4fabea569906e09f2e19ec525cc
Albert Bloomfield <abloomfield@gmail.com>
2016-08-19 17:51:01 -0400

    crop the pdf margins

---
+++ vespers-book/crop.cmd
@@ -0,0 +1,2 @@
+pdfcrop vespers-book.pdf vespers-book-crop-temp.pdf
+gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=vespers-book-crop.pdf vespers-book-crop-temp.pdf &
