388d7d8379706074e938eca4a65f75a24c1b696a
Albert Bloomfield <abloomfield@gmail.com>
2017-08-23 17:05:34 -0400

    update shrink pdf stuff for vespers book

--- vespers-book/build.sh
+++ vespers-book/build.sh
@@ -34,7 +34,7 @@ done
 latexmk vespers-book-main.tex $latexmkopts
 if [ "$?" -eq "0" ] || [ "$force" = "1" ]; then
 	if test `find "vespers-book-main.pdf" -mmin -1` || ! [ -f vespers-book.pdf ] || [ "$force" = "1" ]; then
-		gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=vespers-book.pdf vespers-book-main.pdf &
+		gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=vespers-book.pdf vespers-book-main.pdf &
 	fi
 fi
 
