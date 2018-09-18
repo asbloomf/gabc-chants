1ef5cce410f732bdf0138e491b5f14762c5cb43a
Albert Bloomfield <abloomfield@gmail.com>
2018-09-18 07:33:11 -0400

    a bunch of changes, mostly for the vespers book both my regular edition and birmingham changed commemorations to not get big initial for mag antiphons this should finalize a new version of the book mostly, though I still probably need to change the edition number.

--- vespers-book/crop.cmd
+++ vespers-book/crop.cmd
@@ -1,2 +1,5 @@
 pdfcrop vespers-book.pdf vespers-book-crop-temp.pdf
-gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=vespers-book-crop.pdf vespers-book-crop-temp.pdf &
+gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=vespers-book-crop.pdf vespers-book-crop-temp.pdf &
+
+pdfcrop vespers-book-letter.pdf vespers-book-letter-crop-temp.pdf
+gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=vespers-book-letter-crop.pdf vespers-book-letter-crop-temp.pdf &
