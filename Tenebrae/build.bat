73e6bc344f5ffcab043aae9a083cef39971c397c
Albert Bloomfield <abloomfield@gmail.com>
2019-04-23 15:05:45 -0400

    update some stuff to new gregorio version for tenebrae book setgrefactor is renamed to grechangestaffsize greblockcustos is renamed to greseteolcustos{manual} includescore is renamed to gregorioscore delete generated .tex files from gabc start work on removing the heavily edited gabc files remove gsp-spaces.tex

--- Tenebrae/build.bat
+++ Tenebrae/build.bat
@@ -1,25 +1,25 @@
 latexmk tenebrae_full.tex
 if errorlevel 0 (
 	move tenebrae_full.pdf tenebrae_original.pdf
-	gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=tenebrae_full.pdf tenebrae_original.pdf
+	gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=tenebrae_full.pdf tenebrae_original.pdf &
 )
 
 latexmk tenebrae_thursday.tex
 if errorlevel 0 (
 	move tenebrae_thursday.pdf tenebrae_thursday_original.pdf
-	gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=tenebrae_thursday.pdf tenebrae_thursday_original.pdf
+	gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=tenebrae_thursday.pdf tenebrae_thursday_original.pdf &
 )
 
 latexmk tenebrae_friday.tex
 if errorlevel 0 (
 	move tenebrae_friday.pdf tenebrae_friday_original.pdf
-	gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=tenebrae_friday.pdf tenebrae_friday_original.pdf
+	gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=tenebrae_friday.pdf tenebrae_friday_original.pdf &
 )
 
 latexmk tenebrae_saturday.tex
 if errorlevel 0 (
 	move tenebrae_saturday.pdf tenebrae_saturday_original.pdf
-	gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=tenebrae_saturday.pdf tenebrae_saturday_original.pdf
+	gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=tenebrae_saturday.pdf tenebrae_saturday_original.pdf &
 )
 
 latexmk tenebrae_prophecyTone.tex
@@ -27,7 +27,7 @@ goto end
 
 if errorlevel 0 (
 	move tenebrae_prophecyTone.pdf tenebrae_prophecyTone_original.pdf
-	gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=tenebrae_prophecyTone.pdf tenebrae_prophecyTone_original.pdf
+	gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=tenebrae_prophecyTone.pdf tenebrae_prophecyTone_original.pdf &
 )
 
 :end
