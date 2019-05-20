0f2c602755d46b20fd6caf49577c87ca01ec0f9a
Albert Bloomfield <abloomfield@gmail.com>
2019-05-20 15:18:02 -0400

    update build script (mv command was failing sometimes) fix one small spacing issue

--- Tenebrae/build.sh
+++ Tenebrae/build.sh
@@ -5,30 +5,29 @@ error_exit()
 	exit 1
 }
 
-if latexmk tenebrae_full.tex; then
-	mv tenebrae_full.pdf tenebrae_original.pdf
+if latexmk tenebrae_full.tex && mv tenebrae_full.pdf tenebrae_original.pdf; then
 	gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=tenebrae_full.pdf tenebrae_original.pdf &
+else
+	echo Failed somewhere!
+	read -p "Press a key to continue..." -n1 -s
 fi
 
-if latexmk tenebrae_thursday.tex; then
-	mv tenebrae_thursday.pdf tenebrae_thursday_original.pdf
+if latexmk tenebrae_thursday.tex && mv tenebrae_thursday.pdf tenebrae_thursday_original.pdf; then
 	gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=tenebrae_thursday.pdf tenebrae_thursday_original.pdf &
 fi
 
-if latexmk tenebrae_friday.tex; then
-	mv tenebrae_friday.pdf tenebrae_friday_original.pdf
+if latexmk tenebrae_friday.tex && mv tenebrae_friday.pdf tenebrae_friday_original.pdf; then
 	gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=tenebrae_friday.pdf tenebrae_friday_original.pdf &
 fi
 
-if latexmk tenebrae_saturday.tex; then
-	mv tenebrae_saturday.pdf tenebrae_saturday_original.pdf
+if latexmk tenebrae_saturday.tex mv tenebrae_saturday.pdf tenebrae_saturday_original.pdf; then
 	gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=tenebrae_saturday.pdf tenebrae_saturday_original.pdf &
 fi
 
 latexmk tenebrae_prophecyTone.tex
 exit 0
 
+mv tenebrae_prophecyTone.pdf tenebrae_prophecyTone_original.pdf
 if [ "$?" = "0"]; then
-	mv tenebrae_prophecyTone.pdf tenebrae_prophecyTone_original.pdf
 	gswin64c -dNOPAUSE -dBATCH -dSAFER -sDEVICE=pdfwrite -dCompatibilityLevel=1.3 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile=tenebrae_prophecyTone.pdf tenebrae_prophecyTone_original.pdf &
 fi
